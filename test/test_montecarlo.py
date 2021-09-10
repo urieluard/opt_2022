33bb9815e1eca3244318f37b29370288960bcecf
urieluard <62806181+urieluard@users.noreply.github.com>
2021-09-09 21:03:34 -0500

    Update test_montecarlo.py

--- test/test_montecarlo.py
+++ test/test_montecarlo.py
@@ -1,6 +1,6 @@
 import pytest
 import math
-from src.montecarlo import integracion
+import montecarlo
 import numpy as np
 
 
@@ -11,4 +11,4 @@ def test_integracion():
     a2 = 0
     b2 = 1
     obj = 2 / 3 * (2 / 5 * 2 ** (5 / 2) - 4 / 5)
-    assert integracion(f, a1, b1, a2, b2) - obj < 0.01
+    assert montecarlo.integracion(f, a1, b1, a2, b2) - obj < 0.01
