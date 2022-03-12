bbdaf1593c9b144507d84f46320d82861adfb05f
urieluard <uriel.uard@gmail.com>
2022-03-12 11:12:46 -0600

    plo de paquete

---
+++ lib/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='flumax',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+packages=['flumax'],
+zip_safe=False)
